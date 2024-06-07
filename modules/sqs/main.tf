4762532773c57872cd3076869fdd64e828c52895
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2024-06-07 16:06:14 +0300

    fix: Fix sqs module

--- modules/sqs/main.tf
+++ modules/sqs/main.tf
@@ -1,14 +1,14 @@
 #======SQS==================================================
 
 resource "aws_sqs_queue" "this" {
-  name                      = "${var.base_name}-${var.base_queue_name}-sqs-queue"
+  name = "${var.base_queue_name}-${var.env_name}"
   delay_seconds             = var.delay_seconds
-  max_message_size          = 2048
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
+  max_message_size          = var.max_message_size
+  message_retention_seconds = var.message_retention_seconds
+  receive_wait_time_seconds = var.receive_wait_time_seconds
 
   tags = {
-    Name        = var.base_queue_name
+    Name        = "${var.base_queue_name}-${var.env_name}"
     Environment = var.env_name
   }
 }
--- modules/sqs/variables.tf
+++ modules/sqs/variables.tf
@@ -13,3 +13,18 @@ variable "base_queue_name" {
 variable "delay_seconds" {
   type = number
 }
+
+variable "max_message_size" {
+  type = number
+  default = 2048
+}
+
+variable "message_retention_seconds" {
+  type = number
+  default = 86400
+}
+
+variable "receive_wait_time_seconds" {
+  type = number
+  default = 10
+}
