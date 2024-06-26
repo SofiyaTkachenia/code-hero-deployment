93cce06e7685df187b63618514aa5d7ee7a42d2e
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2024-06-26 22:30:06 +0300

    fix: Fix queues

--- development/queues/variables.tf
+++ development/queues/variables.tf
@@ -7,14 +7,14 @@ variable "base_name" {
   type = string
 }
 
+variable "queue_name" {
+  type = string
+}
+
 variable "env_name" {
   type = string
 }
 
 variable "delay_seconds" {
-  type = string
-}
-
-variable "base_problem_registry_queue_name" {
   type = string
 }
